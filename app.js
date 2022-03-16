83cf2af5bba583c33f59bf7eec84351be039e09f
coding-to-music <connors.tom@gmail.com>
2022-03-16 06:21:40 +0000

    region

--- app.js
+++ app.js
@@ -6,28 +6,27 @@
  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
-
 /*
  * Import required packages.
  * Packages should be installed with "npm install".
  */
-const express = require('express');
-const aws = require('aws-sdk');
+const express = require("express");
+const aws = require("aws-sdk");
 
 /*
  * Set-up and run the Express app.
  */
 const app = express();
-app.set('views', './views');
-app.use(express.static('./public'));
-app.engine('html', require('ejs').renderFile);
+app.set("views", "./views");
+app.use(express.static("./public"));
+app.engine("html", require("ejs").renderFile);
 app.listen(process.env.PORT || 3000);
 
 /*
  * Configure the AWS region of the target bucket.
  * Remember to change this to the relevant region.
  */
-aws.config.region = 'eu-west-1';
+aws.config.region = "us-east-1";
 
 /*
  * Load the S3 information from the environment variables.
@@ -38,33 +37,33 @@ const S3_BUCKET = process.env.S3_BUCKET;
  * Respond to GET requests to /account.
  * Upon request, render the 'account.html' web page in views/ directory.
  */
-app.get('/account', (req, res) => res.render('account.html'));
+app.get("/account", (req, res) => res.render("account.html"));
 
 /*
  * Respond to GET requests to /sign-s3.
  * Upon request, return JSON containing the temporarily-signed S3 request and
  * the anticipated URL of the image.
  */
-app.get('/sign-s3', (req, res) => {
+app.get("/sign-s3", (req, res) => {
   const s3 = new aws.S3();
-  const fileName = req.query['file-name'];
-  const fileType = req.query['file-type'];
+  const fileName = req.query["file-name"];
+  const fileType = req.query["file-type"];
   const s3Params = {
     Bucket: S3_BUCKET,
     Key: fileName,
     Expires: 60,
     ContentType: fileType,
-    ACL: 'public-read'
+    ACL: "public-read",
   };
 
-  s3.getSignedUrl('putObject', s3Params, (err, data) => {
-    if(err){
+  s3.getSignedUrl("putObject", s3Params, (err, data) => {
+    if (err) {
       console.log(err);
       return res.end();
     }
     const returnData = {
       signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
     };
     res.write(JSON.stringify(returnData));
     res.end();
@@ -76,6 +75,6 @@ app.get('/sign-s3', (req, res) => {
  * This function needs to be completed to handle the information in
  * a way that suits your application.
  */
-app.post('/save-details', (req, res) => {
+app.post("/save-details", (req, res) => {
   // TODO: Read POSTed form data and do something useful
 });
